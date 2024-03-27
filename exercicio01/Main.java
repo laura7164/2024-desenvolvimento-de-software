b1807c30d2cf7f4a0b368109b64feac785026692
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-03-27 09:29:59 -0300

    update no main
    fiz um do-while para perguntar pro usuário se ele deseja continuar o código ou não, e mudei o nome da variável da lista para "calculo"

--- exercicio01/Main.java
+++ exercicio01/Main.java
@@ -3,21 +3,30 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner leitor = new Scanner(System.in); 
+
+        String resposta;
+
+        do {
         
-        System.out.print("Digite o valor do primeiro número: ");
-        float numero_1 = leitor.nextInt();
+            System.out.print("Digite o valor do primeiro número: ");
+            float numero_1 = leitor.nextInt();
 
-        System.out.print("Digite o valor do segundo número: ");
-        float numero_2 = leitor.nextInt();
-        leitor.nextLine();
+            System.out.print("Digite o valor do segundo número: ");
+            float numero_2 = leitor.nextInt();
+            leitor.nextLine();
 
-        System.out.print("Escolha a operação desejada (+, -, *, /): ");
-        String operacao = leitor.nextLine();
+            System.out.print("Escolha a operação desejada (+, -, *, /): ");
+            String operacao = leitor.nextLine();
 
-        Calculadora teste = new Calculadora(numero_1, numero_2, operacao);
+            Calculadora calculo = new Calculadora(numero_1, numero_2, operacao);
     
-        System.out.println("Número 1: " + teste.getNumero1());
-        System.out.println("Número 2: " + teste.getNumero2());
-        System.out.println("" + teste.getNumero1() + " " + teste.getOperacao() + " " + teste.getNumero2() + " = " + teste.calcularOperacao());
+            System.out.println("Número 1: " + calculo.getNumero1());
+            System.out.println("Número 2: " + calculo.getNumero2());
+            System.out.println("" + calculo.getNumero1() + " " + calculo.getOperacao() + " " + calculo.getNumero2() + " = " + calculo.calcularOperacao());
+            
+            System.out.print("Deseja fazer outra operação? (s/n): ");
+            resposta = leitor.nextLine();
+            
+        } while (!resposta.equals("n") && !resposta.equals("N"));
     }
 }
