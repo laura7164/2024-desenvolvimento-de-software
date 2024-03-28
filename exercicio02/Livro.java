289810998b31fdb5bbcc00d145c586058111dbf4
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-03-28 10:26:29 -0300

    update na classe livro

--- exercicio02/Livro.java
+++ exercicio02/Livro.java
@@ -1,38 +1,28 @@
-import java.util.Scanner;
 public class Livro {
     
-    private String titulo;
-    private String autor;
-    private String ano;
+    public String titulo;
+    public String autor;
+    public int ano;
 
-    Livro() {
-        System.out.println(">> Armazenando novo livro");
+    public Livro (String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
     }
 
-    Scanner leitor = new Scanner(System.in);
-
-    void lerDados () {
-        System.out.print("Qual é o titulo do livro? ");
-        titulo = leitor.nextLine();
-        System.out.print("Qual é o autor do livro? ");
-        autor = leitor.nextLine();
-        System.out.print("Qual é o ano do livro? ");
-        ano = leitor.nextLine();
-    }
-
-    String getTitulo () {
+    public String getTitulo () {
         return titulo;
     }   
 
-    String getAutor () {
+    public String getAutor () {
         return autor;
     }
 
-    String getAno () {
+    public int getAno () {
         return ano;
     }
 
-    void exibirInfo () {
+    public void exibirInfo () {
         System.out.println("Titulo: " + this.getTitulo());
         System.out.println("Autor: " + this.getAutor());
         System.out.println("Ano: " + this.getAno());
