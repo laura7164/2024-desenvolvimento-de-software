1f98688258654e2289ee3133bc6f048850f876dc
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-03-29 14:50:52 -0300

    update no main
    adicionei uma nova opção no menu para remover um contato (tinha esquecido de botar)

--- exercicio06/Main.java
+++ exercicio06/Main.java
@@ -11,7 +11,8 @@ public class Main {
             System.out.println("################################");
             System.out.println("1. Adicionar novo contato");
             System.out.println("2. Buscar um contato pelo nome");
-            System.out.println("3. Sair");
+            System.out.println("3. Remover contato");
+            System.out.println("4. Sair");
             System.out.println("################################");
             System.out.print("Escolha uma opção: ");
             opcao = leitor.nextInt();
@@ -24,12 +25,15 @@ public class Main {
                     agenda.buscarContato();
                 break;
                 case 3:
+                    agenda.removerContato();
+                break;
+                case 4:
                     System.out.println(">> Saindo...");
                 break;
                 default:
                     System.out.println("Opção inválida.");
                 
             }
-        } while (opcao != 3);
+        } while (opcao != 4);
     }
 }
